4cba571a81537065a468a3620e28fc2d4f7387b7
DaliusLun <80707358+DaliusLun@users.noreply.github.com>
2021-10-05 11:11:40 +0300

    item photo

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"><a href="javascript:history.back()">< Grįžti atgal</a></div>
-                <div class="show__photo">
-                                @if (count($item->photos)>0)
-                                    <img class="photo__big" src="{{asset('/itemPhotos/big/'.$item->photos[0]->name)}}" alt="">
-                                @endif
-                            </div>
+
+                @if(count($item->photos)>0)
+                <div class="container__photos">
+                    <div class="photos__column">
+
+                        @foreach ($item->photos as $photo)
+                            <img class="photo__small" src="{{asset('/itemPhotos/big/'.$photo->name)}}" onclick="myFunction(this);">
+                        @endforeach
+                    </div>
+
+                    <div class="container">
+                    <!-- <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span> -->
+                        <img class="photo__big" id="expandedImg" src="{{asset('/itemPhotos/big/'.$item->photos[0]->name)}}" >
+                    <!-- <div id="imgtext"></div> -->
+                    </div>
+
+                </div>@endif
+
+
+
+
                 <div class="card-body">
                 <table class="table">
                     <thead>
@@ -46,5 +66,17 @@
         </div>
     </div>
 </div>
+<script>
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
 
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("op");
+});
+</script>
 @endsection
